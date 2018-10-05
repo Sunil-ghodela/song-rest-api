7f2ba7165e92fd511a5d305ee20d322404c3bac7
Sunil-ghodela <ss.sunil9255@gmail.com>
2018-10-05 12:32:23 +0530

    adding 1st songs in song api

---
+++ song-v-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/curofy/Desktop/name/project/py/django-1st-week/song-rest-api/song-v-env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
